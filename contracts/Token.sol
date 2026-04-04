354a434f38a02c2f2ecc7edfdb8e0534133f983f
Chris Banning <chrisbanning510@gmail.com>
2026-04-04 16:44:26 -0400

    token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,12 +7,17 @@ contract Token {
     string public name;
     string public symbol = "DAPP";
     uint256 public decimals = 18;
-    uint256 public totalSupply = 1000000 * (10**decimals);
+    uint256 public totalSupply;
 
-    constructor(string memory _name, string memory _symbol, uint56 _totalSupply) {
+    // Track balances
+    mapping(address => uint256) public balanceOf;
+    // Send tokens
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 }
 
